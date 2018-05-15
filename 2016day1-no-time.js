b337e16cce537e17b72250b280edd25b0d404e81
Markus <markusmattsson90@gmail.com>
2018-05-15 16:13:58 +0200

    Add solution to part 2 of 2016 day1

--- 2016day1-no-time.js
+++ 2016day1-no-time.js
@@ -1,17 +1,21 @@
 let arr1 = ["R2", "L3"]; //5 blocks away
 let arr2 = ["R2", "R2", "R2"]; //2 blocks away
 let arr3 = ["R5", "L5", "R5", "R3"]; //12 blocks away
+let arr4 = ["R8", "R4", "R4", "R8"];
 let puzzleInput = ["L1", "L3", "L5", "L3", "R1", "L4", "L5", "R1", "R3", "L5", "R1", "L3", "L2", "L3", "R2", "R2", "L3", "L3", "R1", "L2", "R1", "L3", "L2", "R4", "R2", "L5", "R4", "L5", "R4", "L2", "R3", "L2", "R4", "R1", "L5", "L4", "R1", "L2", "R3", "R1", "R2", "L4", "R1", "L2", "R3", "L2", "L3", "R5", "L192", "R4", "L5", "R4", "L1", "R4", "L4", "R2", "L5", "R45", "L2", "L5", "R4", "R5", "L3", "R5", "R77", "R2", "R5", "L5", "R1", "R4", "L4", "L4", "R2", "L4", "L1", "R191", "R1", "L1", "L2", "L2", "L4", "L3", "R1", "L3", "R1", "R5", "R3", "L1", "L4", "L2", "L3", "L1", "L1", "R5", "L4", "R1", "L3", "R1", "L2", "R1", "R4", "R5", "L4", "L2", "R4", "R5", "L1", "L2", "R3", "L4", "R2", "R2", "R3", "L2", "L3", "L5", "R3", "R1", "L4", "L3", "R4", "R2", "R2", "R2", "R1", "L4", "R4", "R1", "R2", "R1", "L2", "L2", "R4", "L1", "L2", "R3", "L3", "L5", "L4", "R4", "L3", "L1", "L5", "L3", "L5", "R5", "L5", "L4", "L2", "R1", "L2", "L4", "L2", "L4", "L1", "R4", "R4", "R5", "R1", "L4", "R2", "L4", "L2", "L4", "R2", "L4", "L1", "L2", "R1", "R4", "R3", "R2", "R2", "R5", "L1", "L2"];
 
 let travelObj = {
-  //initialize the x and y axis
+  //initialize the x and y axis, this is the current position
   x: 0,
   y: 0,
 
   //initialize direction
   currentDirection: "north",
 
-  goLeft() {
+  //placesBeen is every stop made when getting new instruction, ["x, y", "x, y", ...]
+  placesBeen: ["0, 0"],
+
+  turnLeft() {
     switch(this.currentDirection) {
       case "north":
         this.currentDirection = "west";
@@ -28,7 +32,7 @@ let travelObj = {
     }
   },
 
-  goRight() {
+  turnRight() {
     switch(this.currentDirection) {
       case "north":
         this.currentDirection = "east";
@@ -63,7 +67,13 @@ let travelObj = {
   }
 };
 
-function calculateDistance(arr) {
+function calculateDistance(x, y) {
+  //Distance from starting point to ending point is simply x + y,
+  //if you convert negative integers to positive
+  return Math.abs(x) + Math.abs(y);
+}
+
+function calcPartOne(arr) {
   let leftOrRight = "";
   let numOfBlocks = 0;
 
@@ -73,17 +83,50 @@ function calculateDistance(arr) {
     numOfBlocks = Number(instruction.substring(1, instruction.length));
 
     if (leftOrRight === "L") {
-      travelObj.goLeft();
+      travelObj.turnLeft();
     } else {
-      travelObj.goRight();
+      travelObj.turnRight();
     }
     travelObj.goForward(numOfBlocks);
   });
-
-  //Distance from starting point to ending point is simply x + y,
-  //if you convert negative integers to positive
-
-  return Math.abs(travelObj.x) + Math.abs(travelObj.y);
 }
 
-calculateDistance(puzzleInput);
+function calcPartTwo(arr) {
+  let leftOrRight = "";
+  let numOfBlocks = 0;
+
+  //loop arr and change direction for every instruction
+  for (let i = 0; i<arr.length; i++) {
+    leftOrRight = arr[i].substring(0, 1);
+    numOfBlocks = Number(arr[i].substring(1, arr[i].length));
+
+    if (leftOrRight === "L") {
+      travelObj.turnLeft();
+    } else {
+      travelObj.turnRight();
+    }
+
+    //Move forward one step at the time
+    for (let j = 0; j<numOfBlocks; j++) {
+      travelObj.goForward(1);
+
+      //For every new place we ask ourselves: Have I been here before?
+      let haveIBeenHere = travelObj.placesBeen.find( element => {
+        return element == travelObj.x + ", " + travelObj.y
+      });
+
+      //If yes, we have found the first place, return cooridinates
+      if (haveIBeenHere) {
+        return haveIBeenHere;
+      //else push the cooridinates to placesBeen
+      } else {
+        travelObj.placesBeen.push([travelObj.x + ", " + travelObj.y])
+      }
+    }
+  }
+}
+
+
+//calcPartOne(puzzleInput);
+calcPartTwo(puzzleInput);
+console.log(calculateDistance(travelObj.x, travelObj.y));
